b0b19d79d7332f7770d31f284729c6c93012971c
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2020-06-24 16:57:16 +0800

    Inspector panel toggles and button layout done

--- Assets/Scripts/MainWorkspace.cs
+++ Assets/Scripts/MainWorkspace.cs
@@ -8,22 +8,27 @@ using Newtonsoft.Json.Linq;
 
 public class MainWorkspace : MonoBehaviour
 {
-    public Text totalScene;
-    public Text currentScene;
+    public Text totalScene, currentScene, screenshotTxt, drawingTxt, storyTxt;
     public ToggleGroup ObjTxtColor;
+    public ToggleGroup titleTxtColor;
+    public ToggleGroup contentTxtColor;
     public InputField Objectives;
-    public int[,] objTxtColors = new int[,] {
-        {3, 3, 3},
-        {112, 112, 112},
-        {226, 226, 129},
-        {42, 82, 140},
-        {124, 0, 0}
+    public Scrollbar inspScroll;
+    public Slider screenshot, drawing, story;
+    public float[,] objTxtColors = new float[,] {
+        {0.01f, 0.01f, 0.01f},
+        {0.44f, 0.44f, 0.44f},
+        {0.89f, 0.89f, 0.51f},
+        {0.16f, 0.32f, 0.55f},
+        {0.49f, 0f, 0f}
     };
 
     // Start is called before the first frame update
     void Start()
     {
-
+        functionTogglers("screenshot");
+        functionTogglers("drawing");
+        functionTogglers("story");
     }
 
     // Update is called once per frame
@@ -57,14 +62,55 @@ public class MainWorkspace : MonoBehaviour
         
     }
 
-    public void changeObjectiveTextColor() {
-        Toggle[] objTxtColorToggle = ObjTxtColor.GetComponentsInChildren<Toggle>();
-        foreach(var selectedToggle in objTxtColorToggle) {
-            if(selectedToggle.isOn) {
-                
-                int color = int.Parse((selectedToggle.name).Substring(0, 1));
-                (Objectives.transform.Find("Text").GetComponent<Text>()).color = new Color(objTxtColors[color, 0], objTxtColors[color, 1], objTxtColors[color, 2]);
-            }
+    public void changeObjectiveTextColor(int x) {
+        // Toggle[] objTxtColorToggle = ObjTxtColor.GetComponentsInChildren<Toggle>();
+        // foreach(var selectedToggle in objTxtColorToggle) {
+        //     if(selectedToggle.isOn) {
+        //         int color = int.Parse((selectedToggle.name).Substring(0, 1));
+        //         (Objectives.transform.Find("Text").GetComponent<Text>()).color = new Color(objTxtColors[color, 0], objTxtColors[color, 1], objTxtColors[color, 2], 1f);
+        //     }
+        // }
+        (Objectives.transform.Find("Text").GetComponent<Text>()).color = new Color(objTxtColors[x, 0], objTxtColors[x, 1], objTxtColors[x, 2], 1f);
+    }
+
+    public void showInspScroll() {
+        float scllbr = (inspScroll.GetComponent<CanvasGroup>()).alpha;
+        if(scllbr == 1f) {
+            (inspScroll.GetComponent<CanvasGroup>()).alpha = 0f;
+        } else {
+            (inspScroll.GetComponent<CanvasGroup>()).alpha = 1f;
+        }
+    }
+
+    public void openObjctvBoardChooser() {
+        Debug.Log("Open Objectives Board Chooser");
+    }
+
+    public void functionTogglers(string fnc) {
+        switch(fnc) {
+            case "screenshot":
+                switchState(screenshot, screenshot.value, screenshotTxt);
+                break;
+            case "drawing":
+                switchState(drawing, drawing.value, drawingTxt);
+                break;
+            case "story":
+                switchState(story, story.value, storyTxt);
+                break;
+        }
+    }
+
+    private void switchState(Slider sl, float state, Text txt) {
+        if(state == 1f) {
+            sl.value = 0f;
+            txt.text = "OFF";
+            sl.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = new Color(0.61f, 0.61f, 0.61f, 1f);
+            sl.gameObject.transform.Find("Handle Slide Area").Find("Handle").GetComponent<Image>().color = new Color(0.47f, 0.47f, 0.47f, 1f); 
+        } else {
+            sl.value = 1f;
+            txt.text = "ON";
+            sl.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = new Color(0f, 0.38f, 0f, 1f);
+            sl.gameObject.transform.Find("Handle Slide Area").Find("Handle").GetComponent<Image>().color = new Color(0f, 0.38f, 0f, 1f); 
         }
     }
 }
