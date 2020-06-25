f4163301d3e4823eaee3a0cd901c8963784561ab
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2020-06-25 17:16:43 +0800

    ToDo: Scene thumbnails; Added exit button

--- Assets/Scripts/MainWorkspace.cs
+++ Assets/Scripts/MainWorkspace.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using SceneClassHolder;
 
 public class MainWorkspace : MonoBehaviour
 {
     public Text totalScene, currentScene, screenshotTxt, drawingTxt, storyTxt;
-    public ToggleGroup ObjTxtColor;
-    public ToggleGroup titleTxtColor;
-    public ToggleGroup contentTxtColor;
-    public InputField Objectives;
+    public ToggleGroup ObjTxtColor, titleTxtColor, contentTxtColor;
+    public CanvasGroup popUpOverlay, objctvBoard, sceneBg, titleBoard, contentBoard;
+    public InputField Objectives, title, board;
     public Scrollbar inspScroll;
     public Slider screenshot, drawing, story;
+    public GameObject sceneWndF,sceneWndM, sceneWndL, sceneTmbF, sceneTmbM, sceneTmbL, sceneTmbContainer;
     public float[,] objTxtColors = new float[,] {
         {0.01f, 0.01f, 0.01f},
         {0.44f, 0.44f, 0.44f},
@@ -22,6 +23,7 @@ public class MainWorkspace : MonoBehaviour
         {0.16f, 0.32f, 0.55f},
         {0.49f, 0f, 0f}
     };
+    public SceneClass scenes;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +31,10 @@ public class MainWorkspace : MonoBehaviour
         functionTogglers("screenshot");
         functionTogglers("drawing");
         functionTogglers("story");
+        scenes = new SceneClass();
+        var sceneList = new List<SceneClass.SceneList[]>();
+        
+        
     }
 
     // Update is called once per frame
@@ -51,7 +57,11 @@ public class MainWorkspace : MonoBehaviour
     }
 
     public void addScene() {
-
+        GameObject nScnTmbCon = (GameObject)Instantiate(sceneTmbContainer);
+        RectTransform rt = (RectTransform)nScnTmbCon.transform;
+        float wdt = rt.rect.width;
+        float ht = rt.rect.height + 300f;
+        rt.sizeDelta = new Vector2(wdt, ht);
     }
 
     public void saveScene() {
@@ -63,13 +73,6 @@ public class MainWorkspace : MonoBehaviour
     }
 
     public void changeObjectiveTextColor(int x) {
-        // Toggle[] objTxtColorToggle = ObjTxtColor.GetComponentsInChildren<Toggle>();
-        // foreach(var selectedToggle in objTxtColorToggle) {
-        //     if(selectedToggle.isOn) {
-        //         int color = int.Parse((selectedToggle.name).Substring(0, 1));
-        //         (Objectives.transform.Find("Text").GetComponent<Text>()).color = new Color(objTxtColors[color, 0], objTxtColors[color, 1], objTxtColors[color, 2], 1f);
-        //     }
-        // }
         (Objectives.transform.Find("Text").GetComponent<Text>()).color = new Color(objTxtColors[x, 0], objTxtColors[x, 1], objTxtColors[x, 2], 1f);
     }
 
@@ -83,7 +86,51 @@ public class MainWorkspace : MonoBehaviour
     }
 
     public void openObjctvBoardChooser() {
-        Debug.Log("Open Objectives Board Chooser");
+        objctvBoard.alpha = 1;
+        popUpOverlay.alpha = 1;
+        popUpOverlay.blocksRaycasts = true;
+    }
+
+    public void chooseObjectiveBoard() {
+        objctvBoard.alpha = 0;
+        popUpOverlay.alpha = 0;
+        popUpOverlay.blocksRaycasts = false;
+    }
+
+    public void openSceneBg() {
+        sceneBg.alpha = 1;
+        popUpOverlay.alpha = 1;
+        popUpOverlay.blocksRaycasts = true;
+    }
+
+    public void chooseSceneBg() {
+        sceneBg.alpha = 0;
+        popUpOverlay.alpha = 0;
+        popUpOverlay.blocksRaycasts = false;
+    }
+
+    public void openTitleBoard() {
+        titleBoard.alpha = 1;
+        popUpOverlay.alpha = 1;
+        popUpOverlay.blocksRaycasts = true;
+    }
+
+    public void chooseTitleBoard() {
+        titleBoard.alpha = 0;
+        popUpOverlay.alpha = 0;
+        popUpOverlay.blocksRaycasts = false;
+    }
+
+    public void openContentBoard() {
+        contentBoard.alpha = 1;
+        popUpOverlay.alpha = 1;
+        popUpOverlay.blocksRaycasts = true;
+    }
+
+    public void chooseContentBoard() {
+        contentBoard.alpha = 0;
+        popUpOverlay.alpha = 0;
+        popUpOverlay.blocksRaycasts = false;
     }
 
     public void functionTogglers(string fnc) {
@@ -113,4 +160,38 @@ public class MainWorkspace : MonoBehaviour
             sl.gameObject.transform.Find("Handle Slide Area").Find("Handle").GetComponent<Image>().color = new Color(0f, 0.38f, 0f, 1f); 
         }
     }
+}
+
+namespace SceneClassHolder {
+    public class SceneClass {
+        public class SceneList {
+            public int SceneNumber = 1;
+            public bool IsSelected = true;
+            public class Properties {
+                public class Scene {
+                    public string Background = "";
+                    public string BoardTitle = "";
+                    public string TitleColor = "";
+                    public string ContentColor = "";
+                    public string BoardTitleAnimation = "";
+                    public string BoardAnimation = "";
+                    public string Title = "";
+                    public string Board = "";
+                    public string[] Assets = {};
+                }
+                public class Functions {
+                    public bool Screenshot = false;
+                    public bool Drawing = false;
+                    public bool Story = false;
+                }
+                public class Assessment {
+                    public bool MultipleChoice = false;
+                    public bool Enumeration = false;
+                    public bool MatchingType = false;
+                    public bool FillInTheBlank = false;
+                    public bool Essay = false;
+                }
+            }
+        };
+}
 }
